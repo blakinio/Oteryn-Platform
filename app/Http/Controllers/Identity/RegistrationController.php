ca8eeadaf3d0a551a6e34bb48beec7a75e216657
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-19 09:37:16 +0200

    feat(identity): add registration controller

---
+++ app/Http/Controllers/Identity/RegistrationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Identity;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Identity\RegisterIdentityRequest;
+use App\Identity\Actions\RegisterIdentity;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+
+final class RegistrationController extends Controller
+{
+    public function create(): View
+    {
+        return view('identity.register');
+    }
+
+    public function store(RegisterIdentityRequest $request, RegisterIdentity $registerIdentity): RedirectResponse
+    {
+        $email = $request->validated('email');
+        $password = $request->validated('password');
+
+        if (! is_string($email) || ! is_string($password)) {
+            abort(422);
+        }
+
+        $registerIdentity->execute($email, $password);
+
+        return redirect()
+            ->route('identity.register.create')
+            ->with('status', 'Registration completed.');
+    }
+}
