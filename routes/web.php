69d1274fac910e1d480e34812b28244f69fc5904
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-19 09:38:03 +0200

    feat(identity): add registration routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\Identity\RegistrationController;
 use App\Http\Controllers\PublicGameData\PublicGameDataController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
+
+Route::get('/register', [RegistrationController::class, 'create'])->name('identity.register.create');
+Route::post('/register', [RegistrationController::class, 'store'])
+    ->middleware('throttle:identity-registration')
+    ->name('identity.register.store');
 
 Route::get('/highscores', [PublicGameDataController::class, 'highscores'])->name('game.highscores.index');
 Route::get('/characters/{name}', [PublicGameDataController::class, 'character'])->name('game.characters.show');
