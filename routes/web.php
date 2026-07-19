e630ccc64a8662264e9f1a8305f5699dbbff8fda
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-19 10:00:52 +0200

    feat(identity): add web login and logout routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Identity\RegistrationController;
+use App\Http\Controllers\Identity\SessionController;
 use App\Http\Controllers\PublicGameData\PublicGameDataController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,16 @@ Route::get('/register', [RegistrationController::class, 'create'])->name('identi
 Route::post('/register', [RegistrationController::class, 'store'])
     ->middleware('throttle:identity-registration')
     ->name('identity.register.store');
+
+Route::get('/login', [SessionController::class, 'create'])
+    ->middleware('guest')
+    ->name('identity.login.create');
+Route::post('/login', [SessionController::class, 'store'])
+    ->middleware(['guest', 'throttle:identity-login'])
+    ->name('identity.login.store');
+Route::post('/logout', [SessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('identity.logout');
 
 Route::get('/highscores', [PublicGameDataController::class, 'highscores'])->name('game.highscores.index');
 Route::get('/characters/{name}', [PublicGameDataController::class, 'character'])->name('game.characters.show');
