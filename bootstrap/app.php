31874c49d88e748a2a73d0b71d1bdce89a92b392
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-19 10:16:07 +0200

    chore(identity): isolate static analysis application half

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Middleware\EnsureIdentitySessionIsCurrent;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -13,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/health',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->appendToGroup('web', EnsureIdentitySessionIsCurrent::class);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
