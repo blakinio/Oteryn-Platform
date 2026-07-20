ed44fed10cf2f9535b5bb6a4d51cc7c644f7a2a4
blakinio <75369544+blakinio@users.noreply.github.com>
2026-07-20 11:13:42 +0200

    feat(admin): register permission middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,6 +2,7 @@
 
 use App\Http\Middleware\EnsureConfirmedMfa;
 use App\Http\Middleware\EnsureIdentitySessionIsCurrent;
+use App\Http\Middleware\RequireAdminPermission;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -19,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->appendToGroup('web', EnsureIdentitySessionIsCurrent::class);
         $middleware->alias([
             'mfa.confirmed' => EnsureConfirmedMfa::class,
+            'admin.permission' => RequireAdminPermission::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
